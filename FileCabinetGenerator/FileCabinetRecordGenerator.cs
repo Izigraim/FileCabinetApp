d23dbb4d434558f1274673a2cd98ba5bd931e724
Izigraim <z7635332@gmail.com>
2019-12-20 21:08:59 +0300

    Add FileCabinetRecordGenerator.

---
+++ FileCabinetGenerator/FileCabinetRecordGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using FileCabinetApp;
+
+namespace FileCabinetGenerator
+{
+    public class FileCabinetRecordGenerator
+    {
+        private int startId;
+        private Random random;
+
+        public FileCabinetRecordGenerator(int startId)
+        {
+            this.startId = startId;
+            this.random = new Random();
+        }
+
+        public FileCabinetRecord Generate()
+        {
+            FileCabinetRecord record = new FileCabinetRecord();
+
+            record.Id = this.startId;
+
+            char[] sex = { 'w', 'm' };
+            record.Sex = sex[this.random.Next(0, 2)];
+
+            string name = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i <= this.random.Next(2, 61); i++)
+            {
+                record.FirstName += name[this.random.Next(0, name.Length)];
+            }
+
+            for (int i = 0; i <= this.random.Next(2, 61); i++)
+            {
+                record.LastName += name[this.random.Next(0, name.Length)];
+            }
+
+            record.Salary = Math.Round(Convert.ToDecimal(this.random.NextDouble() * 5000), 2);
+
+            while (true)
+            {
+                try
+                {
+                    record.DateOfBirth = DateTime.Parse(this.random.Next(1, 13).ToString(new CultureInfo("en-US")) + "/" + this.random.Next(1, 32).ToString(new CultureInfo("en-US")) + "/" + this.random.Next(1950, 2020).ToString(new CultureInfo("en-US")), new CultureInfo("en-US"));
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+
+                break;
+            }
+
+            record.Age = Convert.ToInt16(DateTime.Now.Year - record.DateOfBirth.Year);
+
+            this.startId++;
+
+            return record;
+        }
+    }
+}
--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using FileCabinetApp;
 
 namespace FileCabinetGenerator
 {
@@ -8,7 +10,34 @@ namespace FileCabinetGenerator
     {
         private static void Main(string[] args)
         {
-            CommandLineParameters.GeneratorValidation(args);
+            if (CommandLineParameters.GeneratorValidation(args))
+            {
+                List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+
+                int startId = 0;
+                int amount = 0;
+                if (args.Length == 4)
+                {
+                    string[] start = args[3].Split('=');
+                    startId = Convert.ToInt32(start[1], new CultureInfo("en-US"));
+
+                    string[] amountString = args[2].Split('=');
+                    amount = Convert.ToInt32(amountString[1], new CultureInfo("en-US"));
+                }
+
+                if (args.Length == 8)
+                {
+                    startId = Convert.ToInt32(args[7], new CultureInfo("en-US"));
+                    amount = Convert.ToInt32(args[5], new CultureInfo("en-US"));
+                }
+
+                FileCabinetRecordGenerator fileCabinetRecordGenerator = new FileCabinetRecordGenerator(startId);
+
+                for (int i = 0; i < amount; i++)
+                {
+                    list.Add(fileCabinetRecordGenerator.Generate());
+                }
+            }
         }
     }
 }
